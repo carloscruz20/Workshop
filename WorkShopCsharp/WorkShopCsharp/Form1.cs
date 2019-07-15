3e5fbb82140a60a5526743790167d366fa4e6336
carloscruz20 <carlosdv92@gmail.com>
2019-07-15 16:47:36 -0500

    Primero

---
+++ WorkShopCsharp/WorkShopCsharp/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WorkShopCsharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new Pago().Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'workShopDataSet3.empleado' Puede moverla o quitarla según sea necesario.
+            this.empleadoTableAdapter1.Fill(this.workShopDataSet3.empleado);
+            // TODO: esta línea de código carga datos en la tabla 'workShopDataSet.empleado' Puede moverla o quitarla según sea necesario.
+            this.empleadoTableAdapter.Fill(this.workShopDataSet.empleado);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "insert into empleado(nombre,apellido,direccion,cargo) values (@nombre,@apellido,@direccion,@cargo)";
+            SqlCommand cmd = new SqlCommand(query, Conexion.conectar());
+            cmd.Parameters.AddWithValue("@nombre", textBox1.Text);
+            cmd.Parameters.AddWithValue("@apellido", textBox2.Text);
+            cmd.Parameters.AddWithValue("@direccion", textBox3.Text);
+            cmd.Parameters.AddWithValue("@cargo", textBox4.Text);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Se guardo el empleado!!");
+                updateDataGridView("select * from empleado", "empleado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string query = "delete from empleado where id_empleado=@id";
+            SqlCommand cmd = new SqlCommand(query,Conexion.conectar());
+            cmd.Parameters.AddWithValue("@id", comboBox1.Text);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Se Elimino el empleado!!");
+                updateDataGridView("select * from empleado", "empleado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String query = "update empleado set nombre=@nombre , apellido=@apellido , direccion=@direccion , cargo=@cargo where id_empleado=@id";
+            SqlCommand cmd = new SqlCommand(query, Conexion.conectar());
+            cmd.Parameters.AddWithValue("@nombre", textBox1.Text);
+            cmd.Parameters.AddWithValue("@apellido", textBox2.Text);
+            cmd.Parameters.AddWithValue("@direccion", textBox3.Text);
+            cmd.Parameters.AddWithValue("@cargo", textBox4.Text);
+
+            cmd.Parameters.AddWithValue("@id", comboBox1.Text);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Se Actualizo el empleado!!");
+                updateDataGridView("select * from empleado", "empleado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        public void updateDataGridView(String query, String tabla)
+        {
+            SqlDataAdapter ada=new SqlDataAdapter(query,Conexion.conectar());
+            DataSet ds = new DataSet();
+
+            ada.Fill(ds, tabla);
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = tabla;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            string query ="Select * from empleado where nombre like '%" + textBox5.Text + "%'";
+            updateDataGridView(query,"empleado");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
---
+++ WorkShopCsharp/WorkShopCsharp/Pago.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WorkShopCsharp
+{
+    public partial class Pago : Form
+    {
+        public Pago()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            new Form1().Show();
+            this.Hide(); 
+        }
+
+        private void Pago_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'workShopDataSet2.empleado' Puede moverla o quitarla según sea necesario.
+            this.empleadoTableAdapter.Fill(this.workShopDataSet2.empleado);
+            // TODO: esta línea de código carga datos en la tabla 'workShopDataSet1.pago' Puede moverla o quitarla según sea necesario.
+            this.pagoTableAdapter.Fill(this.workShopDataSet1.pago);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Double ihss = 0;
+            Double fosovi = 0;
+            Double neto_pagar = 0;
+            Double total_deduciones = 0;
+            string empelado = comboBox1.Text;
+            Double pago = double.Parse(textBox1.Text);
+
+            if (pago > 7000)
+            {
+                ihss = pago * 0.035;
+            }
+            else
+            {
+                ihss = 7000 * 0.035;
+            }
+
+            fosovi = pago * 0.025;
+            total_deduciones = fosovi + ihss;
+            neto_pagar = pago - total_deduciones;
+
+            string query = "insert into pago(pago,ihss,fosovi,net_pagar,total_deducciones,empleado) values (@pago,@ihss,@fosovi,@neto,@total,@empleado)";
+            SqlCommand cmd = new SqlCommand(query,Conexion.conectar());
+            cmd.Parameters.AddWithValue("@pago",pago);
+            cmd.Parameters.AddWithValue("@ihss", ihss);
+            cmd.Parameters.AddWithValue("@fosovi", fosovi);
+            cmd.Parameters.AddWithValue("@neto", neto_pagar);
+            cmd.Parameters.AddWithValue("@total", total_deduciones);
+            cmd.Parameters.AddWithValue("@empleado", empelado);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Pago Realizado!!");
+                updateDataGridView("Select * from pago","pago");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+        public void updateDataGridView(String query, String tabla)
+        {
+            SqlDataAdapter ada = new SqlDataAdapter(query, Conexion.conectar());
+            DataSet ds = new DataSet();
+
+            ada.Fill(ds, tabla);
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = tabla;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String query = "Select * from pago where empleado=" + comboBox1.Text;
+            updateDataGridView(query, "pago");
+        }
+    }
+}
